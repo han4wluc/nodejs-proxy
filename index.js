e2617a56be29dbb0d24f791298cdc7a5cba556ee
Hanyon <han4wluc@gmail.com>
2015-09-05 15:40:51 +0000

    fix

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-require('./config/express');
+// require('./config/express');
+
+var httpProxy = require('http-proxy');
+
+var options = {
+  changeOrigin: true,
+  target: {
+      https: true
+  }
+}
+
+httpProxy.createServer(443, 'www.googleapis.com/youtube/v3/videos', options).listen(8001, function(){
+  console.log('proxy started on ', new Date());
+});
